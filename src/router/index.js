a5f28b7089add83e7a9f17b2ec4d0d0ae32b3c7d
沫雨熙 <moyuxi@moyuxidndiannao.bbrouter>
2024-07-30 17:06:08 +0800

    loho图标

--- src/router/index.js
+++ src/router/index.js
@@ -3,21 +3,17 @@ import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
+import { Button } from 'vant';
+
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+	component: () => import('../views/home/index.vue')
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  
 ]
 
 const router = new VueRouter({
---
+++ src/views/home/index.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class="home">
+		<div class="logo-page">
+			<van-icon name="chat-o" size="3rem" />
+		</div>
+		<div class="phone">
+			<van-field v-model="text" label="文本" />
+			<van-field v-model="password" type="password" label="密码" />
+		</div>
+	</div>
+</template>
+
+<script>
+</script>
+
+<style scoped lang="scss">
+	body {
+		width: 100%;
+		height: 100%;
+	}
+	.home {
+		border: 2px solid #00ff00;
+		height: 600px;
+		width: 350px;
+		position: absolute;
+		top: 15%;
+		margin: 8% 8%;
+	}
+	.logo-page{
+		margin-top: 20%;
+		text-align: center;
+		font-weight: 900;
+		::v-deep .van-icon{
+			font-size: 3px;
+			font-weight: 900;
+			color: #00ff00;
+		}
+	}
+	::van-icon{
+		
+		
+	}
+</style>
